cb8e737a6a6ce8cbc9ad2e64dc18a4c3f847b110
ArsenSadykov <LoCo10291998@gmail.com>
2024-10-23 03:07:03 +0600

    Ex-2 Done

--- 02/board.ts
+++ 02/board.ts
@@ -1,1 +1,13 @@
-console.log('work');
+let blockChess: string[] = [];
+
+for(let i = 1; i <= 8; i++) {
+    for(let j =1; j <= 8; j++ ){
+        if(i % 2 === j % 2) {
+            blockChess.push('██');
+        } else {
+            blockChess.push('  ');
+        }
+    }
+    console.log(blockChess.join());
+    blockChess = [];
+}
